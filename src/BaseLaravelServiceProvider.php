0603e170f88757a7813b2dd5ba99e64c71934204
Gerson Alves <gersonalvesdev@gmail.com>
2021-10-28 15:44:48 -0300

    -

---
+++ src/BaseLaravelServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace gersonalves\laravelBase;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseLaravelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
