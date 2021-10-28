0313a1e5301ba1d6d306122f340854a3f4256d33
Gerson Alves <gerson.alves@census.inf.br>
2021-10-28 16:24:01 -0300

    adicionado boot

--- src/BaseLaravelServiceProvider.php
+++ src/BaseLaravelServiceProvider.php
@@ -8,8 +8,10 @@ class BaseLaravelServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        require_once('Service/BaseService.php');
         require_once('Service/BaseServiceInterface.php');
+        require_once('Repository/BaseRepositoryInterface.php');
+        require_once('Service/BaseService.php');
+        require_once('Repository/BaseRepository.php');
     }
 
     public function register()
