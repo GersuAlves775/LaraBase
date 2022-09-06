91ef254255053c0f33ea525a3d574cf2f2ff2ca9
Gerson Alves <gersonalvesdev@gmail.com>
2022-09-06 09:40:20 -0300

    fix resource bug

--- src/Traits/ControllerTrait.php
+++ src/Traits/ControllerTrait.php
@@ -15,7 +15,7 @@ trait ControllerTrait
                 return response()->json($this->service->paginate());
 
             $response = $this->service->get();
-            if ($this->resource && class_exists($this->resource) && method_exists($this->resource, 'collection')) {
+            if (method_exists($this->resource, 'collection')) {
                 return $this->resource::collection($response);
             }
 
